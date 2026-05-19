4e5756b809e274f9e685772e42650ebeac29fc79
carlatrapp <81827913+carlatrapp@users.noreply.github.com>
2026-05-19 21:04:27 +0000

    Deploying to gh-pages from @ carlatrapp/carlatrapp.github.io@ef34a7b4b9bb0d5765db46423b9aac988ba5f62e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "carlatrapp/ctrapp.github.io",
+    "data-repo": "carlatrapp/carlatrapp.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
